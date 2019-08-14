faa6a6f05adc19d4143a4c7a5a965cd02b64045d
Sebastian Warnke <sw-github@sebwarnke.com>
2019-08-14 16:45:24 +0200

    add react module show-micro-requirement

---
+++ src/main/webapp/app/modules/show-micro-requirement/show-micro-requirement.tsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+
+class ShowMicroRequirement extends Component {
+  state = {
+    count: 0,
+    nextModule: ''
+  };
+
+  engine = () => {
+    fetch('/rest/engine', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(data => this.setState({ nextModule: data[0].name, ref: true }));
+  };
+
+  render() {
+    if (this.state.nextModule) {
+      return <Redirect to={this.state.nextModule} />;
+    } else {
+      return (
+        <div>
+          <h1>{this.state.nextModule}</h1>
+          <button onClick={this.engine} className="btn btn-primary">
+            Go to Next Step
+          </button>
+        </div>
+      );
+    }
+  }
+}
+
+export default ShowMicroRequirement;
