1aa856517ea27b70da03937b9ee80d2ec0353adc
Niall <niall.p.deehan@gmail.com>
2019-08-14 15:11:09 +0200

    Added Buttons!
    Wooo Buttons

--- src/main/webapp/app/modules/camunda-getstarted/camunda-getstarted.tsx
+++ src/main/webapp/app/modules/camunda-getstarted/camunda-getstarted.tsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
 import { Translate } from 'react-jhipster';
-import { Row, Col, Alert } from 'reactstrap';
-import { Button } from 'react-native';
 
 class CamundaGettingStarted extends React.Component {
+  handleClick(event: MouseEvent) {
+    event.preventDefault();
+    alert(event.currentTarget.tagName); // alerts BUTTON
+  }
+
+  state = {
+    count: 0
+  };
+
+  nextMod = {
+    modeName: 'Nothing'
+  };
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+  engine = () => {
+    fetch('/rest/engine', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    this.nextMod.modeName = 'Set';
+  };
+
   render() {
-    const handlePress = () => false;
-    return <Button onPress={handlePress} title="Red button!" color="red" />;
+    return (
+      <div>
+        <h1>{this.nextMod.modeName}</h1>
+        <button onClick={this.engine}>Create a Springboot Project</button>
+        <button onClick={this.decrement}>Create a Microserice Project</button>
+      </div>
+    );
   }
 }
 
