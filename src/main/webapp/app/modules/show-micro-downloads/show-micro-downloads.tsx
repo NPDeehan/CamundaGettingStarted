50768cc7a83fcb43c84139e4480c5f8fd36926eb
Niall <niall.p.deehan@gmail.com>
2019-08-15 16:44:00 +0200

    Finished requirements for microservices

--- src/main/webapp/app/modules/show-micro-downloads/show-micro-downloads.tsx
+++ src/main/webapp/app/modules/show-micro-downloads/show-micro-downloads.tsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router';
+import { Row, Col, Alert } from 'reactstrap';
 
 class ShowMicroDownloads extends Component {
   state = {
     count: 0,
-    nextModule: ''
+    nextModule: '',
+    ref: false,
+    taskId: '',
+    processInstanceId: '',
+    lang: 'Java'
   };
 
   engine = () => {
@@ -18,20 +23,111 @@ class ShowMicroDownloads extends Component {
       .then(data => this.setState({ nextModule: data[0].name, ref: true }));
   };
 
+  completeTask = () => {
+    fetch(`/rest/task/${this.props.location.state.taskId}/complete`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ variables: { lang: { value: 'Java', type: 'String' } } })
+    }).then(data => {
+      return fetch(`/rest/task?processInstanceId=${this.props.location.state.processInstanceId}`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+      })
+        .then(response => response.json())
+        .then(data => this.setState({ nextModule: data[0].formKey, taskId: data[0].id, ref: true }));
+    });
+  };
+  setLang = event => {
+    console.log(event);
+    this.setState({ lang: event.target.value });
+  };
+
   render() {
-    if (this.state.nextModule) {
-      return <Redirect to={this.state.nextModule} />;
+    if (this.state.ref) {
+      return (
+        <Redirect
+          to={{
+            pathname: `/${this.state.nextModule}`,
+            state: { processInstanceId: this.props.location.state.processInstanceId, taskId: this.state.taskId, lang: this.state.lang }
+          }}
+        />
+      );
     } else {
       return (
-        <div>
-          <h1>{this.state.nextModule}</h1>
-          <button onClick={this.engine} className="btn btn-primary">
-            Go to Next Step
-          </button>
-        </div>
+        <React.Fragment>
+          <div>
+            <h1>Now you've got a choice to make</h1>
+            <h4>Select the programing language you'd like to use for this getting started guide</h4>
+          </div>
+          <div class="btn-group btn-group-toggle" data-toggle="buttons">
+            <label class={this.state.lang === 'Java' ? 'btn btn-primary active' : 'btn btn-primary'}>
+              Java
+              <input checked={this.state.lang === 'Java'} value="Java" type="radio" name="options" id="option1" onChange={this.setLang} />
+            </label>
+            <label class={this.state.lang === 'JavaScript' ? 'btn btn-primary active' : 'btn btn-primary'}>
+              JavaScript
+              <input
+                checked={this.state.lang === 'JavaScript'}
+                value="JavaScript"
+                type="radio"
+                name="options"
+                id="option2"
+                onChange={this.setLang}
+              />
+            </label>
+            <label class={this.state.lang === 'Python' ? 'btn btn-primary active' : 'btn btn-primary'}>
+              I don't care
+              <input
+                checked={this.state.lang === 'Python'}
+                value="Python"
+                type="radio"
+                name="options"
+                id="option3"
+                onChange={this.setLang}
+              />
+            </label>
+          </div>
+          <br />
+          <br />
+          <h4>No matter which language you pick, you're doing to need the Camunda Engine</h4>
+          <br />
+          <br />
+          <div class="alert alert-dismissible alert-info">
+            <button type="button" class="close" data-dismiss="alert">
+              &times;
+            </button>
+            <h4 class="alert-heading">Time to Get Camunda!</h4>
+            <p class="mb-0">
+              <a
+                href="https://camunda.org/release/camunda-bpm/tomcat/7.11/camunda-bpm-tomcat-7.11.0.zip?__hstc=252030934.3ccc2533575ddaceedbbf9babbbf837e.1553783104470.1565305832951.1565877799069.9&__hssc=252030934.1.1565877799069&__hsfp=3195920900"
+                class="alert-link"
+              >
+                download here
+              </a>
+              .
+            </p>
+          </div>
+          <div class="alert alert-dismissible alert-info">
+            <button type="button" class="close" data-dismiss="alert">
+              &times;
+            </button>
+            <h4 class="alert-heading">You should also get Opera</h4>
+            <p class="mb-0">
+              <a href="https://www.opera.com/download" class="alert-link">
+                just because its cool
+              </a>
+              .
+            </p>
+          </div>
+
+          <div>
+            <button onClick={this.completeTask} className="btn btn-primary">
+              Go to Next Step
+            </button>
+          </div>
+        </React.Fragment>
       );
     }
   }
 }
-
 export default ShowMicroDownloads;
