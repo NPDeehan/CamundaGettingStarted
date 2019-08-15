90a0abe1f04c45e8ed3494b95a48c196e0e291dd
Niall <niall.p.deehan@gmail.com>
2019-08-15 10:56:42 +0200

    Actaully fixed it this time

--- src/main/webapp/app/routes.tsx
+++ src/main/webapp/app/routes.tsx
@@ -4,11 +4,11 @@ import Loadable from 'react-loadable';
 
 import Login from 'app/modules/login/login';
 import CamundaGetStarted from 'app/modules/camunda-getstarted/camunda-getstarted';
-import ShowMicroRequirement from 'app/modules/show-micro-requirement/show-micro-requirement';
-import CreateExternalClient from 'app/modules/create-external-client/create-external-client';
-import ShowSpringbootProject from 'app/modules/show-springboot-project/how-springboot-project';
 import ShowMicroDownloads from 'app/modules/show-micro-downloads/show-micro-downloads';
+import ShowMicroRequirement from 'app/modules/show-micro-requirement/show-micro-requirement';
+import ShowSpringbootProject from 'app/modules/show-springboot-project-setup/how-springboot-project-setup';
 import ShowSpringbootRequirement from 'app/modules/show-springboot-requirement/show-springboot-requirement';
+import CreateExternalClient from 'app/modules/create-external-client/create-external-client';
 import Register from 'app/modules/account/register/register';
 import Activate from 'app/modules/account/activate/activate';
 import PasswordResetInit from 'app/modules/account/password-reset/init/password-reset-init';
@@ -37,10 +37,10 @@ const Routes = () => (
   <div className="view-routes">
     <Switch>
       <ErrorBoundaryRoute path="/camunda-getstarted" component={CamundaGetStarted} />
-      <ErrorBoundaryRoute path="/show-micro-requirement" component={ShowMicroRequirement} />
       <ErrorBoundaryRoute path="/create-external-client" component={CreateExternalClient} />
       <ErrorBoundaryRoute path="/show-micro-downloads" component={ShowMicroDownloads} />
-      <ErrorBoundaryRoute path="/show-micro-requirement" component={ShowSpringbootProject} />
+      <ErrorBoundaryRoute path="/show-micro-requirement" component={ShowMicroRequirement} />
+      <ErrorBoundaryRoute path="/show-springboot-project-setup" component={ShowSpringbootRequirement} />
       <ErrorBoundaryRoute path="/show-springboot-requirement" component={ShowSpringbootRequirement} />
       <ErrorBoundaryRoute path="/login" component={Login} />
       <ErrorBoundaryRoute path="/logout" component={Logout} />
